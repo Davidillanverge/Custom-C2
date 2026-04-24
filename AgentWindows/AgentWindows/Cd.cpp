7ac444fbb143a197addec7edf60dd0946dbb9c6d
davidillanverge <davidillanverge@gmail.com>
2026-04-24 22:38:37 +0200

    Fix errors ls/cd

--- AgentWindows/AgentWindows/Cd.cpp
+++ AgentWindows/AgentWindows/Cd.cpp
@@ -1,6 +1,9 @@
 #include "Commands.h"
 
 std::string Cd(std::vector<std::string> arguments) {
+    if (arguments.empty() || arguments[0].empty()) {
+        return std::string("Error: no path provided.");
+    }
     if (SetCurrentDirectoryA(arguments[0].c_str())) {
         return std::string("Current directory has changed.");
     }
--- AgentWindows/AgentWindows/Ls.cpp
+++ AgentWindows/AgentWindows/Ls.cpp
@@ -3,19 +3,21 @@
 std::string Ls(std::vector<std::string> arguments) {
     std::string result;
     WIN32_FIND_DATAA findData;
-    std::string path = arguments[0];
-    if (path.empty()) {
+    std::string path;
+    if (arguments.empty() || arguments[0].empty()) {
         char buffer[MAX_PATH];
         DWORD ret = GetCurrentDirectoryA(MAX_PATH, buffer);
         if (ret == 0 || ret > MAX_PATH) {
-            return {}; // error al obtener directorio actual
+            return {};
         }
         path = buffer;
+    } else {
+        path = arguments[0];
     }
     HANDLE hFind = FindFirstFileA((path + "\\*").c_str(), &findData);
 
     if (hFind == INVALID_HANDLE_VALUE) {
-        return result; // devuelve string vacío si no hay archivos o error
+        return result; // devuelve string vacï¿½o si no hay archivos o error
     }
 
     do {
